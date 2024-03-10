4cc58b393da9b56fc01ac8643766303bac40ded5
Benedicte Emilie Brækken <b@brkn.io>
2024-03-10 08:45:15 +0100

    Add send Slack job

---
+++ app/Jobs/SendMessageToSlackJob.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeEncrypted;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Http;
+
+class SendMessageToSlackJob implements ShouldQueue, ShouldBeEncrypted
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * The number of times the job may be attempted.
+     *
+     * @var int
+     */
+    public $tries = 5;
+
+    /**
+     * The maximum number of unhandled exceptions to allow before failing.
+     */
+    public int $maxExceptions = 3;
+
+    public function __construct(
+        public string $text,
+        public string $webhookUrl
+    ) {
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        $payload = [
+            'content' => $this->text,
+        ];
+        ray($payload);
+        Http::post($this->webhookUrl, $payload);
+    }
+}
